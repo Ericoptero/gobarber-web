37a6217bc26c97e28030f812c42c7fb718aeebf1
Eric Nantes <ericnantes18@gmail.com>
2020-12-18 11:37:38 -0400

    Button e Input isolados em componentes

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
-import { FiLogIn } from 'react-icons/fi';
+import { FiLogIn, FiLock, FiMail } from 'react-icons/fi';
 
 import logoImg from '../../assets/logo.svg';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 import { Container, Content, Background } from './styles';
 
@@ -13,10 +16,15 @@ const SignIn: React.FC = () => (
       <form>
         <h1>Faça seu login</h1>
 
-        <input type="text" placeholder="E-mail" />
-        <input type="password" placeholder="Senha" />
+        <Input icon={FiMail} name="email" type="text" placeholder="E-mail" />
+        <Input
+          icon={FiLock}
+          name="password"
+          type="password"
+          placeholder="Senha"
+        />
 
-        <button type="submit">Entrar</button>
+        <Button type="submit">Entrar</Button>
 
         <a href="esqueci">Esqueci minha senha</a>
       </form>
